4a54d68f303fe44c57d842bd42a3a537a6ac5713
Andrew Anderson <andrewloveskitties@gmail.com>
2012-10-23 21:18:46 -0700

    Added all the Android project files

---
+++ src/com/example/knitknit/ProjectList.java
@@ -0,0 +1,22 @@
+package com.example.knitknit;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class ProjectList extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.projectlist, menu);
+        return true;
+    }
+}
