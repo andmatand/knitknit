098cbfc6b1ed9c547a55973e09686a5942c93bf1
Andrew Anderson <andrewloveskitties@gmail.com>
2013-08-04 21:19:11 -0700

    Implemented counter editor activity, moved license

--- src/com/example/knitknit/CounterEditor.java
+++ src/com/example/knitknit/CounterEditor.java
@@ -1,30 +1,3 @@
-/*
- * Copyright 2012 Andrew Anderson
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 
- *     1. Redistributions of source code must retain the above copyright
- *        notice, this list of conditions and the following disclaimer.
- * 
- *     2. Redistributions in binary form must reproduce the above copyright
- *        notice, this list of conditions and the following disclaimer in the
- *        documentation and/or other materials provided with the distribution.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
 package com.example.knitknit;
 
 import android.app.Activity;
@@ -32,6 +5,7 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.CheckBox;
@@ -39,9 +13,8 @@ import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
-import android.app.Activity;
 
-public class CounterEdit extends Activity {
+public class CounterEditor extends Activity {
     private static final String TAG = "bunny-knitknit-CounterEdit";
     private Counter mCounter;
     private Resources mResources;
@@ -58,11 +31,11 @@ public class CounterEdit extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.counteredit);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
 
-        mCounter = CountingLand.mSelectedCounter;
+        setContentView(R.layout.countereditor);
 
-        Log.w(TAG, "in onCreate, mCounter: " + mCounter);
+        mCounter = CountingLand.selectedCounter;
 
         // If counter ID is still null, there is a problem and we need
         // to get out of here since we can't do anything
@@ -76,6 +49,21 @@ public class CounterEdit extends Activity {
     }
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                //Intent intent = new Intent(this, CountingLand.class);
+                //intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                //startActivity(intent);
+                finish();
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    @Override
     protected void onPause() {
         Log.w(TAG, "in onPause");
         super.onPause();
@@ -83,22 +71,20 @@ public class CounterEdit extends Activity {
     }
 
     private void saveState() {
-        mCounter.setPatternLength(
-                Integer.parseInt(mPatternNumber.getText().toString()));
-
+        mCounter.setPatternLength(Integer.parseInt(mPatternNumber.getText().toString()));
         mCounter.setName(mNameText.getText().toString());
     }
 
     private void setupUI() {
         // Find counter name box
-        mNameText = (EditText)
-            findViewById(R.id.counteredit_name_text);
+        mNameText = (EditText) findViewById(R.id.countereditor_name_text);
 
         Log.w(TAG, "mNameText: " + mNameText);
 
         // Fill name box with counter's name
-        mNameText.append(mCounter.getName());
-
+        if (mCounter.getName() != null) {
+            mNameText.append(mCounter.getName());
+        }
 
         // Create an OnClickListener for the updown radio buttons
         OnClickListener updown_listener = new OnClickListener() {
@@ -106,7 +92,7 @@ public class CounterEdit extends Activity {
                 Log.w(TAG, "clicked radiobutton");
 
                 RadioButton rb = (RadioButton) v;
-                if (rb.getId() == R.id.counteredit_updown_up) {
+                if (rb.getId() == R.id.countereditor_updown_up) {
                     Log.w(TAG, "clicked UP");
                     mCounter.setCountUp(true);
                 } else {
@@ -117,10 +103,8 @@ public class CounterEdit extends Activity {
         };
 
         // Find updown RadioButtons
-        mUpdownUp = (RadioButton)
-            findViewById(R.id.counteredit_updown_up);
-        mUpdownDown = (RadioButton)
-            findViewById(R.id.counteredit_updown_down);
+        mUpdownUp = (RadioButton) findViewById(R.id.countereditor_updown_up);
+        mUpdownDown = (RadioButton) findViewById(R.id.countereditor_updown_down);
 
         // Attach OnClickListeners
         mUpdownUp.setOnClickListener(updown_listener);
@@ -135,41 +119,35 @@ public class CounterEdit extends Activity {
 
 
         // Find pattern checkbox
-        mPatternCheckBox = (CheckBox)
-            findViewById(R.id.counteredit_pattern_checkbox);
+        mPatternCheckBox = (CheckBox) findViewById(R.id.countereditor_pattern_checkbox);
 
         // Check/uncheck the checkbox based on the current setting
-        mPatternCheckBox.setChecked(mCounter.getPatternOn());
+        mPatternCheckBox.setChecked(mCounter.getPatternEnabled());
 
         // Callback function for when the checkbox changes
         mPatternCheckBox.setOnCheckedChangeListener(
-                new CheckBox.OnCheckedChangeListener() {
-                    @Override
-                    public void onCheckedChanged(
-                        CompoundButton v, boolean checked)
-                    {
-                        Log.w(TAG, "checkbox changed");
+            new CheckBox.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton v, boolean checked) {
+                    Log.w(TAG, "checkbox changed");
+                    
+                    // Make text normal/dim
+                    refreshPattern();
 
-                        // Make text normal/dim
-                        refreshPattern();
-
-                        // Enable/disable pattern mode
-                        mCounter.setPatternOn(checked);
-                    }
-                });
+                    // Enable/disable pattern mode
+                    mCounter.setPatternEnabled(checked);
+                }
+            });
 
 
         // Find pattern length number
-        mPatternNumber = (EditText)
-            findViewById(R.id.counteredit_pattern_number);
+        mPatternNumber = (EditText) findViewById(R.id.countereditor_pattern_number);
 
         // Set the number based on the current setting
-        mPatternNumber.append(
-                Integer.toString(mCounter.getPatternLength()));
+        mPatternNumber.append(Long.toString(mCounter.getPatternLength()));
 
         // Find end of pattern text (" rows")
-        mPatternEndText = (TextView)
-            findViewById(R.id.counteredit_pattern_endtext);
+        mPatternEndText = (TextView) findViewById(R.id.countereditor_pattern_endtext);
 
         refreshPattern();
     }
@@ -177,16 +155,16 @@ public class CounterEdit extends Activity {
     private void refreshPattern() {
         // Set color to normal or dim depending on the toggle state
         int color = mResources.getColor(
-                mPatternCheckBox.isChecked() ?
-                R.color.counteredit_text :
-                R.color.counteredit_disabled);
+            mPatternCheckBox.isChecked() ?
+            R.color.countereditor_text :
+            R.color.countereditor_disabled);
 
         // Set color of checkbox text
         mPatternCheckBox.setTextColor(color);
 
         // Enable/disable pattern number EditText
         mPatternNumber.setEnabled(mPatternCheckBox.isChecked());
-
+        
         // Set color of end text
         mPatternEndText.setTextColor(color);
     }
--- src/com/example/knitknit/CountingLand.java
+++ src/com/example/knitknit/CountingLand.java
@@ -1,30 +1,3 @@
-/*
- * Copyright 2012 Andrew Anderson
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 
- *     1. Redistributions of source code must retain the above copyright
- *        notice, this list of conditions and the following disclaimer.
- * 
- *     2. Redistributions in binary form must reproduce the above copyright
- *        notice, this list of conditions and the following disclaimer in the
- *        documentation and/or other materials provided with the distribution.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
 package com.example.knitknit;
 
 import android.app.Activity;
@@ -39,6 +12,7 @@ import android.view.ViewGroup;
 
 public class CountingLand extends Activity {
     private static final String TAG = "knitknit-CountingLand";
+    public static Counter selectedCounter = null;
 
     private DataWrangler mDataWrangler;
     private Project mProject;
@@ -173,6 +147,8 @@ public class CountingLand extends Activity {
             // Highlight thte selected counter
             counter.getWrapper().setSelected(true);
             counter.refreshViews();
+
+            selectedCounter = counter;
         }
     }
 
@@ -213,10 +189,20 @@ public class CountingLand extends Activity {
                     return true;
                 case R.id.delete_counter:
                     mProject.deleteCounter(counter);
-                    mode.finish(); // Action picked, so close the CAB
+
+                    // Close the Contextual Action Bar
+                    mode.finish();
                     return true;
                 case R.id.increase_counter:
                     counter.increase();
+                    return true;
+                case R.id.edit_counter:
+                    // Start the CounterEditor activity
+                    Intent intent = new Intent(CountingLand.this, CounterEditor.class);
+                    startActivity(intent);
+
+                    // Close the Contextual Action Bar
+                    mode.finish();
                     return true;
                 default:
                     return false;
